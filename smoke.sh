d12701f7d4fbf4dbd94bb8dcd6a684b8f8ea9c44
Rob Jahn <robjahn@gmail.com>
2018-10-18 23:49:11 -0400

    Update smoke.sh

--- smoke.sh
+++ smoke.sh
@@ -6,7 +6,7 @@
 SOCK_SHOP_URL=35.231.104.148
 
 echo Running smoke test against $SOCK_SHOP_URL...
-sudo rm -rf results/
+#sudo rm -rf results/
 #sudo rm -rf results_raw/
 #docker rm jmeter-test
 ./launch_test.sh basiccheck.jmx results $SOCK_SHOP_URL 1 10 "SmokeTest"
