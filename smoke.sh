ec55a79428ff1ee47b3d4cbd8843f46aad1b4772
Rob Jahn <robjahn@gmail.com>
2018-10-19 00:41:15 -0400

    Update smoke.sh

--- smoke.sh
+++ smoke.sh
@@ -9,4 +9,4 @@ echo Running smoke test against $SOCK_SHOP_URL...
 #sudo rm -rf results/
 #sudo rm -rf results_raw/
 #docker rm jmeter-test
-./launch_test.sh basiccheck.jmx results $SOCK_SHOP_URL 80 1 10 "SmokeTest"
+./launch_test.sh basiccheck.jmx results $SOCK_SHOP_URL 80 5 10 "SmokeTest"
