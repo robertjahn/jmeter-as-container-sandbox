9a9ef15402342d626a3ea3747e5009c2748c4f09
Rob Jahn <rob.jahn@elyxor.com>
2018-10-18 21:20:07 -0400

    remove rm and k commands

--- smoke.sh
+++ smoke.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # usage: ./quicklaunch.sh <yourserverip> [MyLoadTestName]
 
-export SOCK_SHOP_URL=$(kubectl -n sock-shop get svc front-end --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
+#export SOCK_SHOP_URL=$(kubectl -n sock-shop get svc front-end --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
+
+SOCK_SHOP_URL=35.231.104.148
 
 echo Running smoke test against $SOCK_SHOP_URL...
-sudo rm -rf results/
-sudo rm -rf results_raw/
-docker rm jmeter-test
+#sudo rm -rf results/
+#sudo rm -rf results_raw/
+#docker rm jmeter-test
 ./launch_test.sh basiccheck.jmx results $SOCK_SHOP_URL 1 10 "SmokeTest"
