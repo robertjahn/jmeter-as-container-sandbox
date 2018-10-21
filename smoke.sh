4ca5097bc94aa9d5b77aad0d15876d9caa0a7ad2
Rob Jahn <robjahn@gmail.com>
2018-10-21 19:45:12 -0400

    Update smoke.sh

--- smoke.sh
+++ smoke.sh
@@ -3,7 +3,7 @@
 
 #export SOCK_SHOP_URL=$(kubectl -n sock-shop get svc front-end --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
 
-SOCK_SHOP_URL=35.231.104.148
+SOCK_SHOP_URL=35.231.224.62
 
 echo Running smoke test against $SOCK_SHOP_URL...
 ./launch_test.sh basiccheck.jmx results $SOCK_SHOP_URL 80 5 10 "SmokeTest"
